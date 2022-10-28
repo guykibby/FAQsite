37aa2027e42e767331ce7affba2829339cdd0004
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-10-28 17:05:49 +1300

    modified the migrations, created a get route to fetch data @  Co-authored by oliver.eddy@developersinstitute.co.nz dquote> Co-authored by guy.kibby@developersinstitute.co.nz dquote> Co-authored by jonas.almira@developersinstitute.co.nz dquote> Co-authored by shanthikoyya@developersinstitute.co.nz dquote>

--- api/app.js
+++ api/app.js
@@ -1,13 +1,16 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
-
+const get_db = require("./db");
 app.use(cors())
 app.use(express.json())
 
-app.get("/kia-ora", async (req, res) => {
+app.get("/getData", async (req, res) => {
     try {
-        res.json('kia ora');
+        const db = await get_db();
+        const result = await db.query(`SELECT * FROM questions`);
+        console.log(result.rows);
+        res.json(result.rows);
     } catch (e) {
         return res.status(500).json({ error: e.message });
     }
--- api/migrations/20221028024723-faqui.js
+++ api/migrations/20221028024723-faqui.js
@@ -22,12 +22,20 @@ exports.up = function (db) {
     id SERIAL PRIMARY KEY,
     question_id int REFERENCES questions(id) UNIQUE,
     answer_description  TEXT
-  );`);
+  );
+  
+  INSERT INTO questions (id, question_description) VALUES (1,'what is string interpolation?'),
+  (2,'what is HTML?');
 
+  INSERT INTO answers (id,question_id,answer_description) VALUES (1,2,'HTML standands for Hyper Text Markup Language');
+  `);
   return null;
 };
-
 exports.down = function (db) {
+  db.runSql(`
+  DROP TABLE answers;
+  DROP TABLE questions;
+  `)
   return null;
 };
 
