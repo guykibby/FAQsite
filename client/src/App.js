b652dd61b916d13b2c024058c7106a124749a064
Guy <guykibby@gmail.com>
2022-10-29 16:32:21 +1300

    Add fetch and hot reloading to react app
    Co-Authored-By: jonasalmira <98989341+jonasalmira@users.noreply.github.com>
    Co-Authored-By: C. Scooter Alexander <scooterseoul@gmail.com>
    Co-Authored-By: Shan <52937130+shvinprog@users.noreply.github.com>
    Co-Authored-By: Akshay <98738875+finiky@users.noreply.github.com>
    Co-Authored-By: bryansbizaar <85827531+bryansbizaar@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,15 @@
+const fetchData = async () => {
+  try {
+    const result = await fetch(`${process.env.REACT_APP_API_URL}/getData`);
+    const data = await result.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+fetchData();
+
 const app = () => {
   return <p>kia ora</p>;
 };
