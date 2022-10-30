0386d1121fbd8f6ddb96dbc88cf9c938793a38ab
Guy <guykibby@gmail.com>
2022-10-31 12:59:08 +1300

    Revert to old migration
    Co-Authored-By: Shan <52937130+shvinprog@users.noreply.github.com>
    Co-Authored-By: olle1134 <99058610+olle1134@users.noreply.github.com>
    Co-Authored-By: C. Scooter Alexander <scooterseoul@gmail.com>

--- api/migrations/20221028024723-faqui.js
+++ api/migrations/20221028024723-faqui.js
@@ -17,13 +17,23 @@ exports.setup = function (options, seedLink) {
 exports.up = function (db) {
   db.runSql(`
   CREATE TABLE questions ( id serial PRIMARY KEY, question_description TEXT);
+
+  CREATE TABLE answers (
+    id SERIAL PRIMARY KEY,
+    question_id int REFERENCES questions(id) UNIQUE,
+    answer_description  TEXT
+  );
   
-  INSERT INTO questions (id, question_description) VALUES (1,'What is testing data?');
+  INSERT INTO questions (id, question_description) VALUES (1,'what is string interpolation?'),
+  (2,'what is HTML?');
+
+  INSERT INTO answers (id,question_id,answer_description) VALUES (1,2,'HTML standands for Hyper Text Markup Language');
   `);
   return null;
 };
 exports.down = function (db) {
   db.runSql(`
+  DROP TABLE answers;
   DROP TABLE questions;
   `);
   return null;
