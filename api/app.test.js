d626b0983166423ae79dc3ab87f97b8f078afe4e
Guy <guykibby@gmail.com>
2022-11-01 14:00:57 +1300

    db.end in API test

--- api/app.test.js
+++ api/app.test.js
@@ -2,6 +2,10 @@ const request = require("supertest");
 const app = require("./app");
 
 describe("Q&A API", () => {
+  afterAll(async () => {
+    const db = await get_db();
+    db.end();
+  });
   test("GET /getData should return list of questions and answers with status code of 200", async () => {
     const response = await request(app).get("/getData");
     // .set("Accept", "application/json");
@@ -15,7 +19,7 @@ describe("Q&A API", () => {
     //     question_description: "what is HTML?",
     //   },
     // ]);
-    // expect(response.status).toBe(200);
-    expect(true).toBe(false);
+    expect(response.status).toBe(200);
+    // expect(true).toBe(false);
   });
 });
