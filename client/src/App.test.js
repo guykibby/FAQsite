67d1ee5c6f6669d10c6d72eff80acdcefe6edfd9
Claudette Alexander <claudette.alexander@developersinstitute.co.nz>
2022-11-02 15:49:36 +0900

    Added Router to App in App.test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App.js";
+import { BrowserRouter as Router } from "react-router-dom";
 
 test("renders learn react link", () => {
-  render(<App />);
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
   const linkElement = screen.getByText(/view/i);
   expect(linkElement).toBeInTheDocument();
 });
