0a8a717576b64242373672c382cac8711e5b4b65
Claudette Alexander <claudette.alexander@developersinstitute.co.nz>
2022-11-02 15:55:50 +0900

    Fixed import in App test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import App from "./App.js";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
 test("renders learn react link", () => {
