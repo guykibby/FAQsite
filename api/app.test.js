13434b0cfb94a9b8ce052fc3a0538ce993d2349d
Guy <guykibby@gmail.com>
2022-11-03 00:18:19 +1300

    Update app.test.js to check db connection

--- api/app.test.js
+++ api/app.test.js
@@ -1,27 +1,21 @@
-const request = require("supertest");
-const app = require("./app");
 const get_db = require("./db");
 
-describe("Q&A API", () => {
+describe("API to DB connection", () => {
   afterAll(async () => {
     const db = await get_db();
     db.end();
   });
 
-  test("GET /getData should return list of questions and answers with status code of 200", async () => {
-    const response = await request(app)
-      .get("/getData")
-      .set("Accept", "application/json");
-    expect(response.body).toEqual([
-      {
-        id: 1,
-        question_description: "what is string interpolation?",
-      },
-      {
-        id: 2,
-        question_description: "what is HTML?",
-      },
-    ]);
-    expect(response.status).toBe(200);
+  test("should be active", async () => {
+    const db = await get_db();
+    let response = false;
+    db.connect((err) => {
+      if (err) {
+        response = false;
+      } else {
+        response = true;
+      }
+      expect(response).toBe(true);
+    });
   });
 });
