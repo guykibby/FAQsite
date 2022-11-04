f984015df47e87c2e814cdae056ae1ff77aa61b8
Guy <guykibby@gmail.com>
2022-11-05 09:17:39 +1300

    Revert "Merge branch 'Kitty--HomePage' into Add-tittles-to-page-with-a-simple-className"
    This reverts commit 7da232a1fa23f771c87af28fa2f9cb463c4513c2, reversing
    changes made to 4819eb11ad5098d4e4761604f37cef4b0a290ed9.

--- api/routes/topicsRoute/topics.router.js
+++ api/routes/topicsRoute/topics.router.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const repository = require("./topics.repository");
 
-router.get("/", async (req, res, next) => {
+router.use("/", (req, res, next) => {
   try {
-    const response = await repository.getTopics();
-    res.json(response);
+    throw new Error("Route is under maintenance");
   } catch (err) {
     next(err);
   }
--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,81 +1,14 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
-// the topics array to be fetched from the DB
-let theTopics = [];
-
-// theTopics will be filtered with the use of keys
-const levelKeys = ["year", "term", "topic"];
-
-// the branchs will be determined by the users menu selections
-let branch = "";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  const [level, setLevel] = useState(-1);
-  const navigate = useNavigate(10);
-
-  // Filtering through the array depending on the level and branch the user is on
-  const branchArray = theTopics.filter((x) =>
-    level === 0 ? true : x[levelKeys[level - 1]] === branch
-  );
-
-  // Mapping to get the data we wish to display
-  let displayArray = branchArray.map((x) => x[levelKeys[level]]);
-
-  // Only displayying uniques values
-  displayArray = [...new Set(displayArray)];
-
-  // Fetching the topics data from DB
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch(`${process.env.REACT_APP_API_URL}/topics`);
-      
-      if (result.ok === false) {
-        setLevel(-2)
-        return;
-      }
-      
-      const data = await result.json();
-      theTopics = data;
-      setLevel(0);
-    };
-    fetchData();
-  }, []);
-
-  // handling menu selections
-  const handleClick = (choice) => {
-    if (level === 2) {
-      const topic = theTopics.filter(
-        (e) => e.term === branch && e.topic === choice
-      );
-      navigate(`/questions/${topic[0].id}`);
-    }
-    branch = choice;
-    setLevel((t) => t + 1);
-  };
-
-  if (level === -1) {
-    return <p className="list-item">Loading . . .</p>;
-  }
-
-  if (level === -2) {
-    return <p className="list-item">Oops, something went wrong!</p>;
-  }
-
   return (
-    <>
-      {displayArray.map((e, i) => (
-        <button
-          value={e}
-          key={i}
-          onClick={(e) => handleClick(e.target.value)}
-          className="list-item"
-        >
-          {e}
-        </button>
-      ))}
-    </>
-
+    <div className="main-container">
+      <p className="title">Home Page</p>
+      <Link to={"/questions/" + 1} className="list-item">
+        View Questions
+      </Link>
+    </div>
   );
 };
+
 export default HomePage;
