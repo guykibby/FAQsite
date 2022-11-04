489edf0aa54161a00274dba9e177b00a435afe77
Guy <guykibby@gmail.com>
2022-11-04 14:22:23 +1300

    Update App.test.js

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -8,5 +8,5 @@ test("renders learn react link", () => {
       <App />
     </Router>
   );
-  expect(screen.getByText(/year/i)).toBeInTheDocument();
+  expect(screen.getByText(/ /i)).toBeInTheDocument();
 });
