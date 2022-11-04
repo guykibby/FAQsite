4819eb11ad5098d4e4761604f37cef4b0a290ed9
Guy <guykibby@gmail.com>
2022-11-05 08:45:24 +1300

    Add title class to CSS

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="main-container">
+      <p className="title">Home Page</p>
       <Link to={"/questions/" + 1} className="list-item">
         View Questions
       </Link>
