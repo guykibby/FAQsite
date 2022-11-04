c233055721fa51e085e391c60b73b48e962b4f22
Shan <52937130+shvinprog@users.noreply.github.com>
2022-11-05 10:03:20 +1300

    resolved-review for CI.yaml fail & error handling

--- api/routes/dashboardRouter/dashboard.router.js
+++ api/routes/dashboardRouter/dashboard.router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dashboardRouter = express();
 const dashboardRepository = require("./dashboard.repository");
 
-dashboardRouter.get("/", async (req, res) => {
+dashboardRouter.get("/", async (req, res, next) => {
   try {
     const result = await dashboardRepository.getAllFaqs();
     res.json(result);
