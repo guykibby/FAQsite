90dd1158c3b9b0b5877d5bd29635e082d8fd5f5c
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-11-08 00:03:50 +1300

    added more test

--- client/src/components/EditQuestion.test.js
+++ client/src/components/EditQuestion.test.js
@@ -2,13 +2,15 @@ import { render, screen } from "@testing-library/react";
 import EditQuestion from "./EditQuestion";
 
 describe("EditQuestion Page", () => {
-  test("Renders a EditQuestion Page with checkboxes and button", () => {
+  test("Renders an EditQuestion Page with checkboxes and button", () => {
     render(<EditQuestion />);
+    expect(screen.getByRole("button")).toBeEnabled();
     expect(screen.getByTestId("review-checkbox")).toBeEnabled();
     expect(screen.getByTestId("star-checkbox")).toBeEnabled();
-    expect(screen.getByRole("button")).toBeEnabled();
     expect(screen.getByTestId("review-checkbox")).not.toBeNull();
     expect(screen.getByTestId("star-checkbox")).not.toBeNull();
     expect(screen.getByText(/Delete Question/i)).toBeInTheDocument();
+    expect(screen.getByText(/Review/i)).toBeInTheDocument();
+    expect(screen.getByText(/Star/i)).toBeInTheDocument();
   });
 });
