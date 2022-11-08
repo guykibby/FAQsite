1b114f3d04518e308a79840b8ba007ed81d01bc5
Guy <guykibby@gmail.com>
2022-11-08 21:54:09 +1300

    Add another react homepage test

--- client/src/components/HomePage.test.js
+++ client/src/components/HomePage.test.js
@@ -60,3 +60,44 @@ it("renders topic data", async () => {
   // remove the mock to ensure tests are completely isolated
   global.fetch.mockRestore();
 });
+it("renders topic data", async () => {
+  const fakeTopicsData = [
+    { id: 1, year: "Year 1", term: "Client Side", topic: "HTML" },
+    { id: 9, year: "Year 1", term: "Client Side", topic: "Project1" },
+  ];
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+    Promise.resolve({
+      json: () => Promise.resolve(fakeTopicsData),
+    })
+  );
+
+  // Use the asynchronous version of act to apply resolved promises
+  await act(async () => {
+    render(
+      <Router>
+        <HomePage />
+      </Router>,
+      container
+    );
+  });
+
+  let button = container.querySelector("button");
+  expect(button.textContent).toBe("Year 1");
+
+  act(() => {
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  button = container.querySelector("button");
+  expect(button.textContent).toBe("Client Side");
+
+  act(() => {
+    button.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  button = container.querySelector("button");
+  expect(button.textContent).toBe("HTML");
+
+  // remove the mock to ensure tests are completely isolated
+  global.fetch.mockRestore();
+});
