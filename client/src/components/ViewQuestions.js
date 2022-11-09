3b05d93d3bed0d043ef5f93480c241f05c3cbc11
Bryan <bryan.owens@developersinstitute.co.nz>
2022-11-09 15:44:58 +1300

    Accounted for loading and errors

--- client/src/components/ViewQuestions.js
+++ client/src/components/ViewQuestions.js
@@ -1,25 +1,49 @@
 import { Link, useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
+// import Loader from "../Loader";
+// import ErrorMessage from "../ErrorMessage";
 
 const ViewQuestions = () => {
   const { topicId } = useParams();
   const [questions, setQuestions] = useState([{}]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
+    const abortController = new AbortController();
     const fetchData = async () => {
-      const response = await fetch(
-        `${process.env.REACT_APP_API_URL}/questions/${topicId}`
-      );
-
-      const data = await response.json();
-      setQuestions(data);
+      try {
+        setLoading(true);
+        setError(false);
+        const response = await fetch(
+          `${process.env.REACT_APP_API_URL}/questions/${topicId}`
+        );
+        if (!response.ok) {
+          throw new Error("API Error");
+        }
+        const data = await response.json();
+        if (!abortController.signal.aborted) {
+          setQuestions(data);
+        }
+      } catch (error) {
+        if (!abortController.signal.aborted) {
+          setError(true);
+        }
+      } finally {
+        if (!abortController.signal.aborted) {
+          setLoading(false);
+        }
+      }
     };
 
     fetchData();
+    return () => abortController.abort();
   }, [topicId]);
 
   return (
     <>
+      {loading}
+      {error}
       <p>{questions[0].name}</p>
       {questions.map((e, i) => {
         return (
@@ -31,5 +55,36 @@ const ViewQuestions = () => {
     </>
   );
 };
+
+// const ViewQuestions = () => {
+//   const { topicId } = useParams();
+//   const [questions, setQuestions] = useState([{}]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const response = await fetch(
+//         `${process.env.REACT_APP_API_URL}/questions/${topicId}`
+//       );
+
+//       const data = await response.json();
+//       setQuestions(data);
+//     };
+
+//     fetchData();
+//   }, [topicId]);
+
+//   return (
+//     <>
+//       <p>{questions[0].name}</p>
+//       {questions.map((e, i) => {
+//         return (
+//           <Link key={i} to={"/answers/" + e.id} className="list-item">
+//             {e.description}
+//           </Link>
+//         );
+//       })}
+//     </>
+//   );
+// };
 
 export default ViewQuestions;
