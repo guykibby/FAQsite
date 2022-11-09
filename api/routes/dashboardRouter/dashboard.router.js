8d390330d2576a7f979a963a28f9ba9fce8a0d87
Shan <52937130+shvinprog@users.noreply.github.com>
2022-11-10 12:39:34 +1300

    Router-to display QUESTIONS CRUD using router

--- api/routes/dashboardRouter/dashboard.router.js
+++ api/routes/dashboardRouter/dashboard.router.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dashboardRouter = express();
+const { getNewQuestions } = require("./dashboard.repository");
+
 /**  router localhost:5000/dashboard - to fetch questions from questions table
  * and fetch answers from answers table,
  * it returns newPosts as [[questions],[answers]]
  * */
 dashboardRouter.get("/", async (req, res) => {
   try {
-    res.json("Testing Dashboard Router");
+    const newQuestions = await getNewQuestions();
+    res.json(newQuestions);
   } catch (err) {
     return res.status(500).json(err.message);
   }
