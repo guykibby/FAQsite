dfd489c518f10ae507418a6877703725a1df77cb
Shan <52937130+shvinprog@users.noreply.github.com>
2022-11-10 17:32:09 +1300

    client- questions waiting for review

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,19 +4,22 @@ import { Link } from "react-router-dom";
 /** Fetch API, will fetch questions waiting for review from questions table
  *  and answers waiting for review from answers table
  * which needs to be reviewed by instructor
- * newPosts is an array viriable will holds questions in newPost[0] and answers in newPost[1]
- * to display it on dashboard which are waiting for review by instructor
+ * questions array will hold all questions waiting for review
+ * answers array will hold all questions waiting for review
+ * to display it on dashboard to review by instructor
  * */
 const Dashboard = () => {
-  /* newPosts is an array viriable will holds questions in newPost[0] and answers in newPost[1] */
-  // const [newPosts, setNewPosts] = useState([{}, {}]);
+  /* */
+  const [questions, setQuestions] = useState([]);
+  const [answers, setAnswers] = useState([]);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    setIsLoading(true);
     const fetchData = async () => {
       try {
         const result = await fetch(
-          `${process.env.REACT_APP_API_URL}/dashboard1`
+          `${process.env.REACT_APP_API_URL}/dashboard`
         );
 
         if (result.ok === false) {
@@ -24,8 +27,9 @@ const Dashboard = () => {
           setError(true);
         } else {
           const data = await result.json();
-          // setNewPosts(data);
-          console.log("AAAA : " + JSON.stringify(data));
+          setQuestions(data.questions);
+          setAnswers(data.answers);
+          console.log("questions : " + JSON.stringify(data.questions));
           setIsLoading(false);
         }
       } catch (error) {
@@ -38,11 +42,30 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <div className="list-item">
+    <>
       <h1>Dashboard</h1>
       {isLoading && <p className="loading-list-item">Loading....</p>}
       {error && <p className="error-list-item">Oops, something went wrong!</p>}
-    </div>
+      {/**  questions waiting for review by instructor
+       * line #54- #63 can be done by creating
+       * <Questions /> componet by passing newPosts[0] as props
+       * in <Questions /> componet, by passing individual answer as props
+       * to <Question /> component to reuse it in other modules
+       * but implmented to keep it in sync with other usestories
+       */}
+
+      <h2>Questions</h2>
+      {questions.map((question, index) => (
+        <Link
+          key={index}
+          to={"/editquestion/" + question.id}
+          state={question}
+          className="question-list-item list-item"
+        >
+          {question.description}
+        </Link>
+      ))}
+    </>
   );
 };
 
