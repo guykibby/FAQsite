716210b5160db9eea2f6fdb775b1814753984df6
Shan <52937130+shvinprog@users.noreply.github.com>
2022-11-10 13:15:56 +1300

    API testcases-questions for review with status 200

---
+++ api/routes/dashboardRouter/dashboard.test.js
@@ -0,0 +1,17 @@
+const request = require("supertest");
+const app = require("../../app");
+const get_db = require("../../db");
+const { getNewQuestions, getNewAnswers } = require("./dashboard.repository");
+
+describe("FAQ Dashboard API", () => {
+  afterAll(async () => {
+    const db = await get_db();
+    db.end();
+  });
+  test("GET /dashboard should return questions waiting for review, with status code of 200", async () => {
+    const expectedData = await getNewQuestions();
+    const response = await request(app).get("/dashboard");
+    expect(response.body.questions).toEqual(expectedData);
+    expect(response.status).toBe(200);
+  });
+});
