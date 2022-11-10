32294f5e18a15cb5f598ac7bc088e03136a7943f
olle1134 <99058610+olle1134@users.noreply.github.com>
2022-11-10 13:02:14 +1300

    update api app.js for merge conflict

--- api/app.js
+++ api/app.js
@@ -16,12 +16,12 @@ app.use(express.json());
 
 //routes
 app.use("/topics", getTopicsRouter);
-app.use("/questions/:topicId", getQuestionsRouter);
+app.use("/questions", getQuestionsRouter);
 app.use("/postquestion", postQuestionsRouter);
-app.use("/answers/:questionId", getAnswersRouter);
-app.use("/postanswer/:questionId", postAnswersRouter);
-app.use("/editquestion/:questionId", editQuestionsRouter);
-app.use("/editanswer/:answerId", editAnswersRouter);
+app.use("/answers", getAnswersRouter);
+app.use("/postanswer", postAnswersRouter);
+app.use("/editquestion", editQuestionsRouter);
+app.use("/editanswer", editAnswersRouter);
 app.use("/dashboard", dashboardRouter);
 app.use("/users", usersRouter);
 
