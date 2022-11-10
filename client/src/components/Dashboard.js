c6c1c6d5606c47ba09b189f5b790174984ea413e
Shan <52937130+shvinprog@users.noreply.github.com>
2022-11-10 16:38:35 +1300

    client-fetch API fails-display error message

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -10,26 +10,27 @@ import { Link } from "react-router-dom";
 const Dashboard = () => {
   /* newPosts is an array viriable will holds questions in newPost[0] and answers in newPost[1] */
   // const [newPosts, setNewPosts] = useState([{}, {}]);
-  // const [error, setError] = useState(false);
+  const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const result = await fetch(
-          `${process.env.REACT_APP_API_URL}/dashboard`
+          `${process.env.REACT_APP_API_URL}/dashboard1`
         );
 
         if (result.ok === false) {
-          setIsLoading(true);
-          // setError(true);
-        } else {
           setIsLoading(false);
+          setError(true);
+        } else {
           const data = await result.json();
           // setNewPosts(data);
           console.log("AAAA : " + JSON.stringify(data));
           setIsLoading(false);
         }
       } catch (error) {
+        setError(true);
+        setIsLoading(false);
         console.log("Error fetching products");
       }
     };
@@ -40,6 +41,7 @@ const Dashboard = () => {
     <div className="list-item">
       <h1>Dashboard</h1>
       {isLoading && <p className="loading-list-item">Loading....</p>}
+      {error && <p className="error-list-item">Oops, something went wrong!</p>}
     </div>
   );
 };
