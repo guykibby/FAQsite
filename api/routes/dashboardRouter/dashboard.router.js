6a552d0967138ed5712caf9be40eee81df3e3cd2
Shan <52937130+shvinprog@users.noreply.github.com>
2022-11-10 15:38:05 +1300

    API testcases-questions & answers or []-status 200

--- api/routes/dashboardRouter/dashboard.router.js
+++ api/routes/dashboardRouter/dashboard.router.js
@@ -4,8 +4,14 @@ const { getNewQuestions, getNewAnswers } = require("./dashboard.repository");
 
 /**  router localhost:5000/dashboard - to fetch questions from questions table
  * and fetch answers from answers table,
- * it returns newPosts as [[questions],[answers]]
+ * it returns newPosts as { questions: newQuestions, answers: newAnswers }
  * */
+/** note : Answers tble alone not going to exists with out question table,
+ * reference this error from DB: ""
+ * "ERROR:  update or delete on table "questions" violates
+ *   foreign key constraint "answers_questionid_fkey" on table "answers"
+ *   DETAIL:  Key (id)=(1) is still referenced from table "answers"."
+ */
 dashboardRouter.get("/", async (req, res) => {
   try {
     const newQuestions = await getNewQuestions();
--- api/routes/dashboardRouter/dashboard.test.js
+++ api/routes/dashboardRouter/dashboard.test.js
@@ -8,16 +8,29 @@ describe("FAQ Dashboard API", () => {
     const db = await get_db();
     db.end();
   });
-  test("GET /dashboard should return questions waiting for review, with status code of 200", async () => {
+  test("GET /dashboard should return questions waiting for review or [], with status code of 200", async () => {
     const expectedData = await getNewQuestions();
     const response = await request(app).get("/dashboard");
     expect(response.body.questions).toEqual(expectedData);
     expect(response.status).toBe(200);
   });
-  test("GET /dashboard should return answers waiting for review, with status code of 200", async () => {
+
+  test("GET /dashboard should return answers waiting for review or [], with status code of 200", async () => {
     const expectedData = await getNewAnswers();
     const response = await request(app).get("/dashboard");
     expect(response.body.answers).toEqual(expectedData);
+    expect(response.status).toBe(200);
+  });
+
+  test("GET /dashboard should return questions & answers waiting for review with status code of 200", async () => {
+    let questions = await getNewQuestions();
+    let answers = await getNewAnswers();
+    const expectedBody = { questions, answers };
+    const response = await request(app).get("/dashboard");
+    questions = response.body.questions;
+    answers = response.body.answers;
+    const responseBody = { questions, answers };
+    expect(responseBody).toEqual(expectedBody);
     expect(response.status).toBe(200);
   });
 });
