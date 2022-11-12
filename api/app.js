052840d08ae205b94b307c89f077916df764b55c
Shan <52937130+shvinprog@users.noreply.github.com>
2022-11-12 17:29:19 +1300

    wokring on merge conflicts app.js and package-lock.json

--- api/app.js
+++ api/app.js
@@ -30,7 +30,6 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
   });
-  console.log("GGGG : " + err.status + "   " + err.message);
 });
 
 module.exports = app;
