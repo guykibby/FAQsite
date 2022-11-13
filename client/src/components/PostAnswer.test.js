f3f36730bd043903994f857938300f3976cdd573
Guy <guykibby@gmail.com>
2022-11-14 11:48:30 +1300

    Address minor warnings from esLint
    Co-Authored-By: Akshay <98738875+finiky@users.noreply.github.com>
    Co-Authored-By: jonasalmira <98989341+jonasalmira@users.noreply.github.com>

--- client/src/components/PostAnswer.test.js
+++ client/src/components/PostAnswer.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 import React from "react";
 import { unmountComponentAtNode, render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
