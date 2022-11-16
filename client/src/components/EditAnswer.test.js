c41402c8764929e3b10edea91a13f60514dcbae1
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-11-16 18:27:51 +1300

    added tests to check the client side fetching and rendering descriptions

--- client/src/components/EditAnswer.test.js
+++ client/src/components/EditAnswer.test.js
@@ -51,7 +51,6 @@ describe("EditAnswer Page", () => {
     expect(beforeClick).toBe(!afterClick);
   });
 
-
   test("Alter the value of the Star Checkbox upon click", async () => {
     await act(async () => {
       render(
@@ -71,5 +70,40 @@ describe("EditAnswer Page", () => {
     expect(beforeClick).toBe(!afterClick);
   });
 
-  
+  test("It should display the question and answer description upon visiting the page", async () => {
+    const mockData = {
+      id: 1,
+      description:
+        "Lorem ipsum dolor sit amet. Et molestias voluptatem qui doloremque soluta sit culpa porro et tenetur repellat vel beatae quas id reprehenderit esse.",
+      isstarred: false,
+      isreviewed: false,
+      questionid: 1,
+      userid: null,
+      questiondescription: "What is HTML?",
+    };
+
+    //Mock a succesful fetch response (ie status 200)
+    jest.spyOn(global, "fetch").mockImplementation(() =>
+      Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockData),
+      })
+    );
+    await act(async () => {
+      render(
+        <Router>
+          <EditAnswer />
+        </Router>,
+        container
+      );
+    });
+
+    expect(
+      screen.getByText(
+        `Lorem ipsum dolor sit amet. Et molestias voluptatem qui doloremque soluta sit culpa porro et tenetur repellat vel beatae quas id reprehenderit esse.`
+      )
+    ).toBeInTheDocument;
+    expect(screen.getByText("What is HTML?")).toBeInTheDocument();
+    global.fetch.mockRestore();
+  });
 });
